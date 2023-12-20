c49c4e4efa8efa20c998b0f03b500d718818a67a
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-12-20 23:31:25 +0530

    inttostring

---
+++ 1.String/src/basic/IntString.java
@@ -0,0 +1,11 @@
+package basic;
+
+public class IntString {
+public static void main(String[] args) {
+	int a = 1,b=2,c=3;
+	String  print = String.valueOf(a);
+	String  print2 = String.valueOf(b);
+	String  print3 = String.valueOf(c);
+System.out.println(print+" "+print2+" "+print3);
+}
+}
