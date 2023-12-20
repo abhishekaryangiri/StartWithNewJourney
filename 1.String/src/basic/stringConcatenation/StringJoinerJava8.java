13a00bbe77ebcd6790910ca24e7c49061b5d3d4c
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-12-21 05:13:05 +0530

    java8featurestringjoiner

---
+++ 1.String/src/basic/stringConcatenation/StringJoinerJava8.java
@@ -0,0 +1,14 @@
+package basic.stringConcatenation;
+
+import java.util.StringJoiner;
+
+//StringJoiner: having all the functionality of String.join(), in adv its accept string joiner
+public class StringJoinerJava8 {
+public static void main(String[] args) {
+	StringJoiner a = new StringJoiner(" ");
+	a.add("Abhishek");
+	a.add("Aryan");
+	System.out.println(a.toString());
+	
+}
+}
