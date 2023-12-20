cee0c6698c765d70dd47f59f5784928f4f1b2e7e
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-12-20 23:06:34 +0530

    update pro1

--- 1.String/src/basic/CharString.java
+++ 1.String/src/basic/CharString.java
@@ -1,5 +1,10 @@
 package basic;
 
 public class CharString {
-
+public static void main(String[] args) {
+	char[]  abc = {'m','y','q','t'};
+	String ab = new String(abc);
+	System.out.println(ab.toUpperCase());
+	
+}
 }
