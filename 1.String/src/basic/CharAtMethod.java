b9c863a37c7f101370093f62ca793090991096a1
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2023-12-21 05:49:53 +0530

    char at

---
+++ 1.String/src/basic/CharAtMethod.java
@@ -0,0 +1,11 @@
+package basic;
+
+//ChatAt: char at specific pos
+public class CharAtMethod {
+public static void main(String[] args) {
+	String a = "Abhishek";
+	System.out.println(a.charAt(0));
+	System.out.println(a.charAt(5));
+	System.out.println(a.toUpperCase());
+}
+}
