e25398f03a50bed4fadb540564a265b134b34010
Abhishek Aryan <bittoogoswami@gmail.com>
2024-01-03 23:59:36 +0530

    Update CharAtMethod.java

--- 1.String/src/basic/CharAtMethod.java
+++ 1.String/src/basic/CharAtMethod.java
@@ -3,6 +3,7 @@ package basic;
 //ChatAt: char at specific pos
 public class CharAtMethod {
 public static void main(String[] args) {
+	
 	String a = "Abhishek";
 	System.out.println(a.charAt(0));
 	System.out.println(a.charAt(1));
